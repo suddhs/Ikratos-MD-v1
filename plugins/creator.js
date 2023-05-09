be4245af13cc974cdfccf0bca4848d28799f3627
suddhs <114920896+suddhs@users.noreply.github.com>
2023-05-09 19:59:38 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -8,7 +8,7 @@ let name = await conn.getName(who)
     [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `ɪɴɴᴏᴄᴇɴᴛ ᴍᴏʀᴇ ᴛʜᴀɴ ɢᴏᴏɢʟᴇ`, `ikratosofc@gmail.com`, `🇵🇰 Pakistan`, `🐙 https://github.com/xIKRATOSx`, `👑 ᴏᴡɴᴇʀ ɪᴋʀᴀᴛᴏs ʙᴏᴛ`],
     [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ`, `📵 ᴅᴏɴᴛ sᴘᴀᴍ/ᴄᴀʟʟ ᴍᴇ 😢`, `ɴᴏᴛʜɪɴɢ`, `🇵🇰 Pakistan`, `🐙 https://github.com/xIKRATOSx/Ikratos-MD-v1`, `🤖 ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ𝟷`]
   ], fkontak)
-  await m.reply(`Listen baby @${m.sender.split('@')[0]}, That's My Owner Don't be over, Okay? 😃\nTry to be polite\nDon\t call him or you will be ignored`)
+  await m.reply(`Hi cuddh mk itim @${m.sender.split('@')[0]}, මූ තමා මගේ owner, 😃\nඕන වුනොත් contact කරපන් වට්සැප්`)
   } 
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
